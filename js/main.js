5f5e4e195389e0ac6d7ec4c781bf566cfa80ca61
Roman Kuzovlev <rkuzovlev@gmail.com>
2016-05-18 03:31:33 +0600

    Add articles and some fixes

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	var body = $('body');
+	var scroller = $('.scroll span');
+
+	$('.scroll').on('click', function() {
+		var offset = $('.donate').offset();
+		body.animate({scrollTop: offset.top}, 1000);
+	});
+
+	var showHideScroller = function(){
+		if (body.scrollTop() > 100){
+			scroller.addClass('hidden');
+		} else {
+			scroller.removeClass('hidden');
+		}
+	}
+
+	$(document).scroll(showHideScroller);
+
+	showHideScroller();
+});
